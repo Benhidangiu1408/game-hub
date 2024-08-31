b231074914d47d92b31c5f41610ee4ff40eb0038
TRAN NHI <nhil31032@gmail.com>
2024-08-31 18:59:42 +0700

    Build a Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        //     `"header header"
+        // "aside main"
+        // "aside footer"`
+        base: `"header" "main" "footer" `,
+        lg: `"header header"
+         "aside main"
+         "aside footer"`,
+      }}
+    >
+      <GridItem area="header" bg="blue">
+        header
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="pink">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="red">
+        main
+      </GridItem>
+      <GridItem area="footer" bg="yellow">
+        footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;

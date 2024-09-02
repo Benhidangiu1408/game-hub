18b1ecf95faeed209627998021caac55b8389595
TRAN NHI <nhil31032@gmail.com>
2024-09-02 23:03:09 +0700

    static genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,5 @@
 import useData from "./useData";
-
+import genres from '../data/genres'
 
 export interface Genres{
     id:number,
@@ -8,6 +8,6 @@ export interface Genres{
 }
 
 
-const useGenres=()=>useData<Genres>('/genres')
+const useGenres=()=>({data:genres,errors:null,isLoading:false})
 
 export default useGenres
